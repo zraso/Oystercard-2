db07a5d0471e66ec5ed3089d834e70ebcb56d358
kevindhanna <kevindhanna@gmail.com>
2019-09-23 19:36:35 +0100

    Add Oystercard class and balance instance variable

---
+++ README.md
@@ -0,0 +1,6 @@
+`In order to use public transport
+As a customer
+I want money on my card`
+
+Objects | Messages
+Card    | Money / Balance
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
-describe Oystercard do
+require 'oystercard'
 
+describe Oystercard do
+  describe '#balance' do
+    context 'balance is 0' do
+      it 'displays 0' do
+        expect(subject.balance).to eq 0
+      end
+    end
+  end
 end
