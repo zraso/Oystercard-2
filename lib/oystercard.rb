ea43697374efa79d5e39c0252d11afaaf35a5615
Vijay Kurian <kurianvijay7@gmail.com>
2019-09-24 15:21:33 +0100

    Added fail condition to top_up method, tests failing

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -7,7 +7,10 @@ class Oystercard
   end
 
   def top_up(amount)
+    # @balance = @balance + amount
+    raise 'Error: Cannot top up, balance exceeds Maximum Balance £90' if @balance >= 90
     @balance = @balance + amount
+    p @balance
   end
 
   # def balance
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,4 +13,12 @@ describe Oystercard do
         expect(oyster.balance).to eq 5
       end
   end
+
+  describe '#maximum_balance' do
+    it 'throws an error if balance is greater than £90' do
+      oyster = Oystercard.new
+      oyster.top_up(89)
+      expect(oyster.top_up(1)).to raise_error 'Error: Cannot top up, balance exceeds Maximum Balance £90'
+    end
+  end
 end
